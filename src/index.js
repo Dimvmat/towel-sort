e8ab9784dc3afe373f5dd47fd9c3f7171ff9d144
dimvmat <102551294+Dimvmat@users.noreply.github.com>
2023-08-08 21:39:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix) {
+        let array = [];
+        if (matrix.length > 0) {
+            for (let i in matrix) {
+                if (i % 2 === 0) {
+                    array = array.concat(matrix[i]);
+                } else {
+                    array = array.concat(matrix[i].reverse());
+                }
+            }
+        }
+        return array;
+    } else {
+        return []
+    }
 }
